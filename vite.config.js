9ba563fc42e5dcb4954c0c800a5b1ea0f13b838a
codenub666 <smurf4812@gmail.com>
2025-03-17 07:01:02 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Auth-manager/", // ✅ Set this to your repo name (case-sensitive)
+});
